00750be435698fb97d6fa2125eb27d8e7c1d500e
Ilari <rantaplanta36@hotmail.com>
2016-12-10 13:43:00 +0200

    Ekat tiedostot

---
+++ src/ct60a2411/harkkatyö/CT60A2411Harkkatyö.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ct60a2411.harkkatyö;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Ilari
+ */
+public class CT60A2411Harkkatyö extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Pakettiautomaatti.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
