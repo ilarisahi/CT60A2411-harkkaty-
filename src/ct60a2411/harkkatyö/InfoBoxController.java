8088379ca8237dec60a5946752b0432bde708fcc
Ilari <rantaplanta36@hotmail.com>
2016-12-10 20:57:53 +0200

    class info added

--- src/ct60a2411/harkkatyö/InfoBoxController.java
+++ src/ct60a2411/harkkatyö/InfoBoxController.java
@@ -11,6 +11,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 /**
@@ -22,13 +23,37 @@ public class InfoBoxController implements Initializable {
 
     @FXML
     private Button returnBut;
+    @FXML
+    private Label class1info1;
+    @FXML
+    private Label class2info1;
+    @FXML
+    private Label class3info1;
+    @FXML
+    private Label class1info2;
+    @FXML
+    private Label class2info2;
+    @FXML
+    private Label class3info2;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        class1info1.setText("Nopea toimitus\n"
+                + "rikkoutumisvaara\n"
+                + "toimitus max. 150km päähän");
+        class1info2.setText("Max. paino 20kg\n"
+                + "mitat 60 x 50 x 30 (cm)");
+        class2info1.setText("Särkymätön kuljetus\n"
+                + "toimitus mihin vaan\n");
+        class2info2.setText("Max. paino 10kg\n"
+                + "mitat 20 x 20 x 30 (cm)");
+        class3info1.setText("Hidas kuljetus\n"
+                + "rikkoutumisvaara\n");
+        class3info2.setText("Max. paino 200kg\n"
+                + "mitat 200 x 200 x 200 (cm)");
     }    
 
     @FXML
